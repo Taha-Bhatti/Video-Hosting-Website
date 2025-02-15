b5e1496f995aa2635013e9d267c57d9869a2c97f
Taha <muhammadtahabhatti1@gmail.com>
2025-02-15 06:14:26 -0500

    custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -2,15 +2,43 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_Name } from "./constants.js";
-
 import connectDb from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: './.env'
 })
 
 connectDb()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`app listining to port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) =>{
+    console.log(`mongo failed to connect`,err);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
+
 (async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
