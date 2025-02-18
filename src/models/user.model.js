b8d1738d77a3153b960f5b034e8014367567a385
Taha <muhammadtahabhatti1@gmail.com>
2025-02-18 11:56:25 -0500

    user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,7 +65,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 userSchema.methods.generateAccessToken = function (){
-    jwt.sign(
+    return jwt.sign(
         {
             _id : this._id,
             username : this.username,
@@ -80,14 +80,14 @@ userSchema.methods.generateAccessToken = function (){
 }
 
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id : this._id,
             
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn :process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn :process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
